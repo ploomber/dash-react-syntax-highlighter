61e5b176c7d3909c7f0cbb47642243460d895d13
Eduardo Blancas <github@blancas.io>
2024-09-26 12:10:00 -0600

    allow copying code

--- src/lib/components/DashReactSyntaxHighlighter.react.js
+++ src/lib/components/DashReactSyntaxHighlighter.react.js
@@ -1,14 +1,31 @@
-import React from 'react';
+import React, { useState } from 'react';
 import PropTypes from 'prop-types';
 import { Prism as SyntaxHighlighter } from 'react-syntax-highlighter';
 import { dark, coy, okaidia, twilight, solarizedlight } from 'react-syntax-highlighter/dist/esm/styles/prism';
 
 /**
  * DashReactSyntaxHighlighter is a component for syntax highlighting.
- * It takes properties for the code to highlight, the programming language, and the style.
+ * It takes properties for the code to highlight, the programming language, the style, and an optional parameter to allow copying the snippet to the clipboard.
  */
 const DashReactSyntaxHighlighter = (props) => {
-    const { id, code, language, styleName } = props;
+    const { id, code, language, styleName, allowCopy } = props;
+    const [buttonLabel, setButtonLabel] = useState('Copy');
+    const [buttonStyle, setButtonStyle] = useState({
+        position: 'absolute',
+        top: '10px',
+        right: '10px',
+        backgroundColor: 'darkgray',
+        color: 'white',
+        border: 'none',
+        padding: '5px 10px',
+        textAlign: 'center',
+        textDecoration: 'none',
+        display: 'inline-block',
+        fontSize: '12px',
+        margin: '4px 2px',
+        cursor: 'pointer',
+        borderRadius: '4px'
+    });
 
     const styleMap = {
         dark: dark,
@@ -20,8 +37,24 @@ const DashReactSyntaxHighlighter = (props) => {
 
     const style = styleMap[styleName] || okaidia;  // Default to 'okaidia' if styleName is not recognized
 
+    const copyToClipboard = () => {
+        navigator.clipboard.writeText(code);
+        setButtonLabel('Copied!');
+        setTimeout(() => {
+            setButtonLabel('Copy');
+        }, 1000);
+    };
+
     return (
-        <div id={id}>
+        <div id={id} style={{ position: 'relative' }}>
+            {allowCopy && (
+                <button
+                    onClick={copyToClipboard}
+                    style={buttonStyle}
+                >
+                    {buttonLabel}
+                </button>
+            )}
             <SyntaxHighlighter language={language} style={style}>
                 {code}
             </SyntaxHighlighter>
@@ -31,7 +64,8 @@ const DashReactSyntaxHighlighter = (props) => {
 
 DashReactSyntaxHighlighter.defaultProps = {
     language: 'javascript',
-    styleName: 'okaidia'  // Set default style to 'okaidia'
+    styleName: 'okaidia',  // Set default style to 'okaidia'
+    allowCopy: true  // Enable copy to clipboard by default
 };
 
 DashReactSyntaxHighlighter.propTypes = {
@@ -55,6 +89,11 @@ DashReactSyntaxHighlighter.propTypes = {
      * Options are: 'dark', 'coy', 'okaidia', 'twilight', 'solarizedlight'
      */
     styleName: PropTypes.string,
+
+    /**
+     * Optional parameter to allow copying the snippet to the clipboard.
+     */
+    allowCopy: PropTypes.bool,
 
     /**
      * Dash-assigned callback that should be called to report property changes
